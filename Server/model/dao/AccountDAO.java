d35e0bbd4ec0c89f0c8824bf0193ae550551e8f2
Tri Dao <minhtri1396@gmail.com>
2017-05-26 23:49:59 +0700

    Added: add AccountDAO and UserDAO classes to interact with account and user's information database

---
+++ Server/model/dao/AccountDAO.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+public class AccountDAO {
+    
+    // Check the mapping of username and password
+    // Return id of player if it's correct, otherwise -1
+    public int isCorrect(String username, String password) {
+        return -1;
+    }
+    
+}
---
+++ Server/model/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.datatype.Player;
+
+public class UserDAO {
+    
+    // Get information of user has the playerID
+    public Player getInfo(int playerID) {
+        return null;
+    }
+    
+}
