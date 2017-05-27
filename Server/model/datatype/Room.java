ebc0fb9c05c300551d6bb98a89e2a5fa8146683c
Tri Dao <minhtri1396@gmail.com>
2017-05-27 19:19:39 +0700

    Updated: add restTime variable to room, room will be destroyed when the resTime equals zero

--- Server/model/datatype/Room.java
+++ Server/model/datatype/Room.java
@@ -1,16 +1,34 @@
 package model.datatype;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Room {
+    
+    private static final int MAX_LIVE_TIME = 60; // 60 seconds
     
     private final int id;
     private String name;
     private byte numberPlayers;
+    private final AtomicInteger restTime; // if restTime == 0, the room will be detroyed
 
     public Room(int id, String name) {
         this.id = id;
         this.name = name;
+        this.restTime = new AtomicInteger(MAX_LIVE_TIME);
     }
-
+    
+    public void resetRestTime() {
+        restTime.set(MAX_LIVE_TIME);
+    }
+    
+    public void decreaseRestTime() {
+        restTime.decrementAndGet();
+    }
+    
+    public int getRestTime() {
+        return restTime.get();
+    }
+    
     public int getId() {
         return id;
     }
