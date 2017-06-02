13f9feeb6c580ee24866f29e1d9978a2d239e8d8
Vu <nxvu699134@gmail.com>
2017-06-03 03:42:35 +0700

    update

--- Client/main/Main.java
+++ Client/main/Main.java
@@ -1,9 +1,21 @@
-package main;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
 
-public class Main {
-
-    public static void main(String[] args) {
-        
+import Client.viewer.*;
+/**
+ *
+ * @author InNovaTioN
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+//        DEMO1.Show();
+        new BoardGUI().setVisible(true);
     }
     
 }
